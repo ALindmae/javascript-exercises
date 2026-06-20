8389dd32c53b4db9e83ab6fd19c090cb9ebae5f2
ALindmae <113936984+ALindmae@users.noreply.github.com>
2026-06-20 09:45:08 -0300

    Add 'contains' exercise solution

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,13 @@
-const contains = function() {
-  
+const contains = function (obj, value, match = false) {
+  for (const val of Object.values(obj)) {
+    if (val === value || (Number.isNaN(value) && Number.isNaN(val)))
+      return (match = true);
+    if (val && typeof val === "object") {
+      match = contains(val, value, match);
+    }
+  }
+  return match;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
