4f07680ac288ceb3e9268a89461b0deb5101139f
ALindmae <113936984+ALindmae@users.noreply.github.com>
2026-06-22 15:35:30 -0300

    add totalintegers exercise solution

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,20 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (item, total = 0, validated = false) {
+  if (!validated) {
+    if (!(item !== null && typeof item === "object")) {
+      return undefined;
+    }
+  }
+  if (Number.isInteger(item)) {
+    return 1;
+  }
+  if (Array.isArray(item)) {
+    for (const value of item) total += totalIntegers(value, 0, true);
+  } else if (item != null && typeof item === "object") {
+    for (const value of Object.values(item))
+      total += totalIntegers(value, 0, true);
+  }
+  return total;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
