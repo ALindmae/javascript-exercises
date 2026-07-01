3f6762d41c97584fde07d0a9f76eadb3b820efa0
ALindmae <113936984+ALindmae@users.noreply.github.com>
2026-07-01 16:09:29 -0300

    add permutations exercise solution

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,18 @@
-const permutations = function() {
-  
+const permutations = (nums, sol = [], res = []) => {
+  if (nums.length === 1) sol.push([...nums]);
+  else if (res.length === nums.length) {
+    sol.push([...res]);
+  } else {
+    for (let i = 0; i < nums.length; i++) {
+      if (!res.includes(nums[i])) {
+        res.push(nums[i]);
+        permutations(nums, sol, res);
+        res.pop();
+      }
+    }
+  }
+  return sol;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
