610f2e58ccd0f68fe0f57c712d34980028a7716a
ALindmae <113936984+ALindmae@users.noreply.github.com>
2026-07-02 14:56:31 -0300

    add pascal exercise solution

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,17 @@
-const pascal = function() {
-  
+const pascal = function (rowNo) {
+  if (rowNo === 1) {
+    return [1];
+  } else {
+    const lastRow = pascal(rowNo - 1);
+    lastRow.push(0);
+    lastRow.unshift(0);
+    const currentRow = [];
+    for (i = 0; i < lastRow.length - 1; i++) {
+      currentRow.push(lastRow[i] + lastRow[i + 1]);
+    }
+    return currentRow;
+  }
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
